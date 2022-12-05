5a038f13262490c6498cbf5acb17fb6314f84bc8
Croak-Croak <azamat.kalmykov07@gmail.com>
2022-12-05 15:39:03 +0300

    commit No. 123133

--- server.js
+++ server.js
@@ -4,18 +4,9 @@ import cors from "cors";
 import morgan from "morgan";
 import colors from "colors";
 import enableWs from "express-ws";
-
-// dataBase config
-import { connectDB } from "./config/dataBase.js";
-
-// routes
-import userRoutes from "./routes/userRoutes.js";
-import roomRoutes from "./routes/roomRoutes.js";
 import expressAsyncHandler from "express-async-handler";
 
 dotenv.config();
-
-connectDB();
 
 const app = express();
 const WSServer = enableWs(app);
@@ -28,25 +19,22 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/users", userRoutes);
-app.use("/api/room/", roomRoutes);
-app.post('/api/room/:id',expressAsyncHandler(async(req,res)=>{
-  try {
-    const {roomId,isPause,progress} = req.body;
-    
-  }catch (error){
-    console.log(error);
-    return res.status(500,"error")
-  }
-}))
+app.post(
+  "/api/room/:id",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const { roomId, isPause, progress } = req.body;
+    } catch (error) {
+      console.log(error);
+      return res.status(500, "error");
+    }
+  })
+);
 app.ws("/api/room/:id/", function (ws, req) {
   ws.on("open", function (message) {});
 
   ws.on("message", function (message) {
-    // console.log(message);
     const data = JSON.parse(message);
-    const ownerData ={};
-    
     switch (data.method) {
       case "connection":
         connectionHandler(ws, data);
@@ -55,9 +43,8 @@ app.ws("/api/room/:id/", function (ws, req) {
         broadcastConnection(ws, data);
         break;
       case "sync":
-        syncHandler(ws,data);
+        syncHandler(ws, data);
         break;
-  
     }
   });
 });
@@ -78,26 +65,11 @@ function connectionHandler(ws, data) {
   broadcastConnection(ws, data);
 }
 
-
 function broadcastConnection(ws, data) {
   aWss.clients.forEach((client) => {
     if (client.roomId === data.roomId) {
-      // if (data.owner ==true) {
-      //   ws.isPause = data.isPause;
-      //   ws.progress = data.progress
-      // }
-      if (data.ownerId ==client.id && client.owner == true){
-        console.log(client)
-        // ws.isPause = client.isPause;
-        // ws.progress = client.progress
-        // data.isPause = client.isPause;
-        // data.progress = client.progress;
+      if (data.ownerId == client.id && client.owner == true) {
       }
-      // if(client.owner ==true){
-      //   data.isPause = client.isPause;
-      //   data.progress = client.progress;
-      // }
- 
       client.send(
         JSON.stringify({
           ...data,
